a6a11ae8ee5c8f9358dc28f5f4caa1b2bbaad101
llz <34785889+mitseng@users.noreply.github.com>
2020-10-31 23:02:31 +0800

    .

---
+++ src/Mito_Dataset.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 31 22:15:22 2020
+
+@author: zll
+"""
+
+from torch.utils import data
+
+
+class Mito_Dataset(data.Dataset):
+    def __init__(self):
+        ...
---
+++ src/patch_extraction.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 31 22:23:21 2020
+
+@author: qq623
+"""
+
